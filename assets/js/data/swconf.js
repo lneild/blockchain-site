049450bae8aff268f05b313d0360a89307ec1eea
lneild <lneild@users.noreply.github.com>
2025-06-12 18:49:07 +0000

    deploy: 3f8d0cbcaa05875be20ca6c97e966627d7b4dd45

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749754054',resources: [
+    cacheName: 'chirpy-1749754146',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
